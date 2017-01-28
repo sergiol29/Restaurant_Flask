a4f64b7bea968fc07978e04ef91e4fb215f4fd22
Sergio Lopez Ballesteros <sergio@MacBook-Pro-de-Sergio.local>
2017-01-28 14:34:47 +0100

    versión final

--- init.py
+++ init.py
@@ -291,4 +291,6 @@ def estadisticas():
 
 if __name__ == "__main__":
     app.debug = True
-    app.run(host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+    #app.run(host='0.0.0.0')
