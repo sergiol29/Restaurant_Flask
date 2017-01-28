9baa3970115925ed317447bf4afb5aa90b9ef133
Sergio Lopez Ballesteros <sergio@MacBook-Pro-de-Sergio.local>
2017-01-28 15:02:26 +0100

    versión final

--- feedParserRss.py
+++ feedParserRss.py
@@ -6,8 +6,7 @@ import feedparser
 # Init Parse RSS
 def getRSS():
     # URL RSS
-    #urlRss = 'https://ep00.epimg.net/rss/tecnologia/portada.xml'
-    urlRss = '{{ url_for('static', filename='rss/noticias.xml') }}'
+    urlRss = 'http://ep00.epimg.net/rss/tecnologia/portada.xml'
     #feed = feedparser.parse('http://ep00.epimg.net/rss/tecnologia/portada.xml')
 
     # Parse RSS
